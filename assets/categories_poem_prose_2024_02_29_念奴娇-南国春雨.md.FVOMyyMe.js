71f972d15a76b1003e7068c713f31c31961396f5
MMF9702 <43512851+MMF9702@users.noreply.github.com>
2024-03-02 11:19:10 +0000

    Deploying to pages from @ MMF9702/MMF9702.github.io@689e24534611d56765195dc15bfc1dbd0fff6b5b 🚀

---
+++ assets/categories_poem_prose_2024_02_29_念奴娇-南国春雨.md.FVOMyyMe.js
@@ -0,0 +1,9 @@
+import{_ as c}from"./chunks/ArticleMetadata.Sb1DYAHo.js";import{_ as l,D as _,o as r,c as i,I as d,w as p,k as e,a as m,b as u,e as f}from"./chunks/framework.FVQzxbLi.js";import"./chunks/md5.RtphNWHi.js";const x=JSON.parse('{"title":"念奴娇-南国春雨","description":"","frontmatter":{"title":"念奴娇-南国春雨","author":"MMF","date":"2024/02/29 15:41","categories":["诗词与散文"],"tags":["宋词"]},"headers":[],"relativePath":"categories/poem_prose/2024/02/29/念奴娇-南国春雨.md","filePath":"categories/poem_prose/2024/02/29/念奴娇-南国春雨.md","lastUpdated":1709285813000}'),h={name:"categories/poem_prose/2024/02/29/念奴娇-南国春雨.md"},g=e("h1",{id:"念奴娇-南国春雨",tabindex:"-1"},[m("念奴娇.南国春雨 "),e("a",{class:"header-anchor",href:"#念奴娇-南国春雨","aria-label":'Permalink to "念奴娇.南国春雨"'},"​")],-1),$=e("pre",null,[e("code",null,`登高俯瞰，看江山万里，尽是春处。
+春雨银丝三千万，搅碎寒意无数。
+大江大河，顿然醒悟，化作游龙开路。
+西风虽烈，难却少年心志。
+笑看挫折起伏，举樽醉饮，往事成云雾。
+酒醒千愁都不见，回看尘落馨附。
+斜阳垂暮，冷月上枝，隐隐照前路。
+恍然思绪，心事随雨化春泥。
+`)],-1);function k(t,C,N,B,M,V){const n=c,s=_("ClientOnly");return r(),i("div",null,[g,d(s,null,{default:p(()=>{var a,o;return[(((a=t.$frontmatter)==null?void 0:a.aside)??!0)&&(((o=t.$frontmatter)==null?void 0:o.showArticleMetadata)??!0)?(r(),u(n,{key:0,article:t.$frontmatter},null,8,["article"])):f("",!0)]}),_:1}),$])}const y=l(h,[["render",k]]);export{x as __pageData,y as default};
